a03fbc269c5b9cc21313595a6949d4b198130e96
Rubén Leobardo Reséndiz Tapia <71289263+Vadcko@users.noreply.github.com>
2020-09-17 19:05:07 -0700

    Add files via upload

---
+++ Peliculas/Peliculas/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peliculas
+{
+    class Pelicula
+    {
+        //ATRIBUTOS
+        public string nombre;
+        public string calificacion;
+        public string año;
+
+        //Metodos
+
+        //todos los metodos deben estar dentro de una clase
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelicula p1 = new Pelicula();
+            p1.nombre = "Sueño de fuga";
+            p1.calificacion = "9,2";
+            p1.año = "1994";
+
+
+            Pelicula p2 = new Pelicula();
+            p2.nombre = "El Padrino";
+            p2.calificacion = "9,1";
+            p2.año = "1972";
+
+            Console.WriteLine("Top Rated Movies");
+            Console.WriteLine(p1.nombre + " " + p1.calificacion);
+            Console.WriteLine(p2.nombre + " " + p2.calificacion);
+            Console.ReadKey();
+        }
+    }
+}
